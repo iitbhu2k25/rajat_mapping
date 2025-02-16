085fa4b5c84cd4bf9124a5c08421435adceb02d3
Your Name <you@example.com>
2025-02-16 12:54:21 +0530

    done

--- stp/models.py
+++ stp/models.py
@@ -1,5 +1,38 @@
 from django.db import models
 
+class State(models.Model):
+    state_id=models.IntegerField()
+    state_name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.state_name
+
+class District(models.Model):
+    district_id=models.IntegerField()
+    district_name=models.CharField(max_length=30)
+    state_id=models.ForeignKey(State, on_delete=models.CASCADE)
+
+class Sub_district(models.Model):
+    id = models.AutoField(primary_key=True)
+    subdistrict_id=models.IntegerField()
+    subdistrict_name=models.CharField(max_length=100)
+    district_id=models.ForeignKey(District, on_delete=models.CASCADE)
+
+class Villages(models.Model):
+    village_id=models.IntegerField()
+    village_name=models.CharField(max_length=100)
+    subdistrict_id=models.ForeignKey(Sub_district, on_delete=models.CASCADE)
+    sewage_gap=models.DecimalField(max_digits=20, decimal_places=10)
+    mean_temperature=models.DecimalField(max_digits=20, decimal_places=10)
+    mean_rainfall=models.DecimalField(max_digits=20, decimal_places=10)
+    number_of_tourists=models.IntegerField()
+    water_quality_index=models.DecimalField(max_digits=20, decimal_places=10)
+    number_of_asi_sites=models.IntegerField()
+    gdp_at_current_prices=models.DecimalField(max_digits=20, decimal_places=10)
+    
+
+
+
 
 class Weight(models.Model):
     sewage_gap = models.FloatField()
